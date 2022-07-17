d50194ff242a6a0fbc54fa8b6e78f35a44fc3460
mahsa hmd <m.ahmadpoor@hami.blue>
2022-07-17 17:15:21 +0430

    remove template engine files

--- book-list.js
+++
@@ -1,8 +0,0 @@
-const listWrapper = document.querySelector('.list-wrapper');
-
-const bookCard = () => {
-    const bookName = document.createElement('p');
-    bookName.classList.add('book-name');
-    const booktitle = document.createElement('p');
-    booktitle.classList.add('book-title');
-}
